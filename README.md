ac3bbd8f072fcbb52c3aeb7a50621b1537084e46
Peng Liu <liupeng@imscv.com>
2016-03-17 10:06:48 +0800

    Update file name pattern and add sha256 check and corresponding file deletion.

--- README.md
+++ README.md
@@ -8,3 +8,6 @@ simply rum `python download.py`, all images are downloaded under `download`.
 ### Notice
 
 Since I want to vertify the image and extract face region from the images, opencv-python package needed. If you are on Windows, this [url](http://www.lfd.uci.edu/~gohlke/pythonlibs/) can be very helpful to install packages.
+
+### What's new in this fork?
+Using FirstName_LastName_imageidx_faceidx.jpg as file name as recorded in the txt. Use sha256 value to check if the downloaded image is correct. If not delete it. If certain existing image file doesn't pass its sha256 value check, then delete it and retry to download it.
--- download.py
+++ download.py
@@ -13,28 +13,46 @@ if not exists(RESULT_ROOT):
     os.mkdir(RESULT_ROOT)
 
 
-def download((names, urls, bboxes)):
+def download((names, imageids, faceids, urls, bboxes, sha256s)):
     """
         download from urls into folder names using wget
     """
 
     assert(len(names) == len(urls))
     assert(len(names) == len(bboxes))
+    assert(len(names) == len(sha256s))
+    assert(len(names) == len(imageids))
+    assert(len(names) == len(faceids))
 
     # download using external wget
-    CMD = 'wget -c -t 1 -T 3 "%s" -O "%s"'
+    # CMD = 'http_proxy=http://localhost:8118 https_proxy=http://localhost:8118 wget -c -t 1 -T 5 "%s" -O "%s"'
+    CMD = 'wget -c -t 1 -T 5 "%s" -O "%s"'
     for i in range(len(names)):
         directory = join(RESULT_ROOT, names[i])
         if not exists(directory):
             os.mkdir(directory)
-        fname = hashlib.sha1(urls[i]).hexdigest() + '.jpg'
+        # fname = hashlib.sha256(urls[i]).hexdigest() + '.jpg'
+        # fname = sha256s[i] + '.jpg'
+        fname = "%s_%d_%d.jpg" % (names[i], imageids[i], faceids[i])
         dst = join(directory, fname)
         print "downloading", dst
+        if exists(dst) and sha256s[i] != hashlib.sha256(
+                    open(dst, 'rb').read()).hexdigest():
+            os.remove(dst)
         if exists(dst):
             print "already downloaded, skipping..."
             continue
         else:
             res = os.system(CMD % (urls[i], dst))
+            if res != 0:
+                os.remove(dst)
+                print "Download failed for %s" % dst
+                continue
+            elif exists(dst) and sha256s[i] != hashlib.sha256(
+                    open(dst, 'rb').read()).hexdigest():
+                os.remove(dst)
+                print "Delete incomplete file for %s" % dst
+                continue
         # get face
         face_directory = join(directory, 'face')
         if not exists(face_directory):
@@ -48,7 +66,7 @@ def download((names, urls, bboxes)):
             face = img[bboxes[i][1]:bboxes[i][3], bboxes[i][0]:bboxes[i][2]]
             cv2.imwrite(face_path, face)
             # write bbox to file
-            with open(join(directory,'_bboxes.txt'), 'a') as fd:
+            with open(join(directory, '_bboxes.txt'), 'a') as fd:
                 bbox_str = ','.join([str(_) for _ in bboxes[i]])
                 fd.write('%s %s\n' % (fname, bbox_str))
 
@@ -61,33 +79,53 @@ if __name__ == '__main__':
             names = []
             urls = []
             bboxes = []
+            imageids = []
+            faceids = []
+            sha256s = []
             for line in fd.readlines():
                 components = line.split('\t')
                 assert(len(components) == 6)
                 name = components[0].replace(' ', '_')
+                imageid = int(components[1])
+                faceid = int(components[2])
                 url = components[3]
                 bbox = [int(_) for _ in components[4].split(',')]
+                sha256 = components[-1][:-1]
                 names.append(name)
+                imageids.append(imageid)
+                faceids.append(faceid)
                 urls.append(url)
                 bboxes.append(bbox)
+                sha256s.append(sha256)
         # every name gets a task
         last_name = names[0]
         task_names = []
+        task_imageids = []
+        task_faceids = []
         task_urls = []
         task_bboxes = []
+        task_sha256s = []
         tasks = []
         for i in range(len(names)):
             if names[i] == last_name:
                 task_names.append(names[i])
+                task_imageids.append(imageids[i])
+                task_faceids.append(faceids[i])
                 task_urls.append(urls[i])
                 task_bboxes.append(bboxes[i])
+                task_sha256s.append(sha256s[i])
             else:
-                tasks.append((task_names, task_urls, task_bboxes))
+                tasks.append((task_names, task_imageids, task_faceids,
+                              task_urls, task_bboxes, task_sha256s))
                 task_names = [names[i]]
+                task_imageids = [imageids[i]]
+                task_faceids = [faceids[i]]
                 task_urls = [urls[i]]
                 task_bboxes = [bboxes[i]]
+                task_sha256s = [sha256s[i]]
                 last_name = names[i]
-        tasks.append((task_names, task_urls, task_bboxes))
+        tasks.append((task_names, task_imageids, task_faceids,
+                      task_urls, task_bboxes, task_sha256s))
 
         pool_size = multiprocessing.cpu_count()
         pool = multiprocessing.Pool(processes=pool_size, maxtasksperchild=2)
